f48fabfb0815efadfd0e46a477b333f11b5d91b3
Andmedoctopus <andmed999@gmail.com>
2021-07-23 01:02:31 +0300

    Fix seq in room model

--- src/room_booking/infrastructure/models/room.py
+++ src/room_booking/infrastructure/models/room.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, MetaData, String, Table
+from sqlalchemy import Column, Integer, MetaData, Sequence, String, Table
 
 metadata = MetaData()
 
@@ -6,7 +6,7 @@ metadata = MetaData()
 room_table = Table(
     "room",
     metadata,
-    Column("id", Integer, Sequence("room_id_seq", metadata=meta), primary_key=True),
+    Column("id", Integer, Sequence("room_id_seq", metadata=metadata), primary_key=True),
     Column("name", String),
     Column("floor", Integer, nullable=False),
     Column("number", Integer, nullable=False),
