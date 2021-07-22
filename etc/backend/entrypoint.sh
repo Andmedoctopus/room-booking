4eaa0dd71c408909bfd9471a6fd464686efd2085
Andmedoctopus <andmed999@gmail.com>
2021-07-23 00:59:03 +0300

    Add DB configuration

--- etc/backend/entrypoint.sh
+++ etc/backend/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-exec ./wait-for-it.sh $POSTGRES_HOST:$POSTGRES_PORT -- "$@"
+exec ./wait-for-it.sh $POSTGRES_APP_HOST:$POSTGRES_APP_PORT -- "$@"
--- src/room_booking/config.py
+++ src/room_booking/config.py
@@ -1,12 +1,26 @@
-from pydantic import BaseConfig, BaseSettings
+from pydantic import BaseSettings
 
 
-class Server(BaseConfig):
-    PORT: int = 5050
+class DatabaseSettings(BaseSettings):
+    HOST: str
+    PORT: int
+    USER: str
+    PASSWORD: str
+    DB: str
+
+    @property
+    def url(self):
+        return "postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}".format(
+            username=self.USER,
+            password=self.PASSWORD,
+            host=self.HOST,
+            port=self.PORT,
+            database=self.DB,
+        )
 
     class Meta:
-        prefix = 'SERVER_'
+        prefix = "POSTGRES_APP"
 
 
 class Settings(BaseSettings):
-    server: Server = Server()
+    db: DatabaseSettings = DatabaseSettings()
